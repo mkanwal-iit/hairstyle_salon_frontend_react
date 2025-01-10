223272459096be7e35bdd1158942e65bbeb0a79b
mkanwal@iit <70186630+mkanwal-iit@users.noreply.github.com>
2025-01-09 21:36:58 -0600

    update header

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,26 +5,16 @@ export function Header() {
   const homeBackgroundUrl = "https://i.etsystatic.com/16895977/r/il/69634d/4897961404/il_570xN.4897961404_7839.jpg";
   return (
     <header>
-      <nav>
+      <nav className="flex flex-col items-center justify-center text-center">
         <Link to="/">Home</Link> | <Link to="/signup">Signup</Link> |<Link to="/login">Login</Link> |{" "}
         <Link to="/services">Services</Link> |<Link to="/appointments/new">New Appointment</Link> |
         <LogoutLink />
         <div>
-          <p style={{ fontSize: "4rem", fontWeight: "bold" }}>Welcome to My Amazing Salon</p>
+          <p className="text-4xl font-bold">Welcome to My Amazing Salon</p>
           <img
             src={homeBackgroundUrl}
             alt="Home"
-            style={{
-              backgroundImage: `url(${homeBackgroundUrl})`,
-              backgroundPosition: "center",
-              height: "80vh", // This sets the height of the image background to fill the screen
-              color: "white", // Sets text color to white for better readability
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              textAlign: "center",
-            }}
+            className="bg-cover bg-center h-[80vh] text-white flex flex-col justify-center items-center text-center"
           />
         </div>
       </nav>
